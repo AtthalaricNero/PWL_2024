1f30d5f85318089a79e8e6cc3bdb009f421b2c6f
Atthalaric Nero Muchtar <atthalaricnero@gmail.com>
2025-02-17 14:27:29 +0700

    jobsheet2_routing_nero

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM:2341720215 Nama: Atthalaric Nero';
+});
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function(
+    $postId, $commentId) {
+        return 'Pos ke-'. $postId . " Komentar ke-: " .$commentId;
+});
+
+Route::get('/articles/{id}', function($artikelId) {
+    return 'Halaman Artikel dengan ID ' . $artikelId;
+});
+
+Route::get('/user/{name?}', function($name='John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile', function() {
+    return 'Ini adalah halaman profile';
+})->name('profile');
